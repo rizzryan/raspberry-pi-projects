56d6224f876ddbb5ce78f319f66ca76b32516089
Ryan Welch <ryanwelch.nsa@gmail.com>
2017-06-12 20:01:11 -0400

    Fixed format issue.

--- sensehat/accelerometer.py
+++ sensehat/accelerometer.py
@@ -12,4 +12,5 @@ while True:
 	y = round(y, 0)
 	z = round(z, 0)
 
-	sense.show_message("X-axis orientation: {0}, Y-axis orientation: {1}, Z-axis orientation: {2}", scroll_speed = 0.05).format(x, y, z)
+	message = "X-axis orientation: {0}, Y-axis orientation: {1}, Z-axis orientation: {2}".format(x, y, z)
+	sense.show_message(message, scroll_speed = 0.05)
